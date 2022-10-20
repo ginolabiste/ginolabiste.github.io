73fd23d736c556f7104c25837a93673d2e773c9e
Gino Labiste <gino.labiste@gmail.com>
2022-10-20 13:58:16 +0800

    simple homepage

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,37 @@
 </script>
 
 <template>
-  <div class="col">
-    <h2>Hello world.</h2>
+  <div class="main">
+    <div class="col">
+      <h2>Hello world.</h2>
+    </div>
+    <div class="col">
+      <a class="btn" target="_blank" href="https://ginolabiste.github.io/todolist-vue/">Todo List Sample</a> 
+    </div>
+    <div class="col">
+      <a  class="btn" target="_blank" href="https://ginolabiste.github.io/weight-tracker-vue/">Weight Tracker Sample</a>
+    </div>
+    <div class="col">
+      <small>by: Gino Labiste</small>
+    </div>
+    
   </div>
 </template>
 
 <style scoped>
+  .main{
+    text-align: start;
+  }
+  .btn{
+    padding: 15px;
+    color: #fff;
+    background-color: #e63946;
+  }
+  .btn:hover{
+    background-color: #1d3557;
+  }
+  .col{
+    padding: auto;
+    display: grid;
+  }
 </style>
